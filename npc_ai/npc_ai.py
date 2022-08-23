51e658d53b63376bcf7e24f2299954152a428baf
shas3 <shas3@LAPTOP-8BU9S0G8>
2022-08-23 18:27:45 +0530

    Add project files.

---
+++ npc_ai/npc_ai.py
@@ -0,0 +1,30 @@
+from typing import Optional
+import random
+
+
+class map_objects:
+    def __init__(self) -> None:
+        width = 1600 # in metres
+        height = 1600 # in metres
+        animal_1 = [] # co-ordinates of animal 1 (list of tuples) # sample data : [(1202,25,1),(100,10,2)] - (co-ordinate, roam, number)
+
+    def animal_1(self, spawn=0, roam = 20, num=1) -> list:
+        """
+        spawn - co-ordinates of spawn location
+        roam - maximum roam area (square) if object is chased it will dissapear after running to the corner of the area
+        num - number of 
+        """
+        if spawn==0:
+            return self.animal_1
+
+        elif spawn >0:
+            if spawn
+            
+
+
+
+    
+        
+
+
+
